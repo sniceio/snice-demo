65c1809bc81095e6204fed29e4f69008816fd2aa
Jonas Borjesson <borjesson.jonas@gmail.com>
2023-04-25 10:56:01 -0700

    triggering build

--- src/main/java/io/snice/demo/http/Http202.java
+++ src/main/java/io/snice/demo/http/Http202.java
@@ -8,14 +8,15 @@ import static io.snice.testing.http.HttpDsl.get;
 import static io.snice.testing.http.HttpDsl.http;
 import static io.snice.testing.http.check.HttpCheckSupport.status;
 
-public class Http201 extends Simulation {
+public class Http202 extends Simulation {
 
     {
         final var http = http().baseUrl("http://local.honeypot.snice.io");
-        final var get = get("/happy").check(status().is(200));
-        final var scenario = scenario("Simple GET").execute(get);
-
+        final var get = get("/error").check(status().is(500));
+        final var scenario = scenario("Simple GET error").execute(get);
         setUp(scenario).protocols(http);
+
+
     }
 
     public static void main(final String... args) throws Exception {
